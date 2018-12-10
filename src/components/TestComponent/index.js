d28c46c9ac806dde171696359abff77528c7712a
YBerch <yuriiberch@gmail.com>
2018-12-10 08:01:35 +0200

    first commit

---
+++ src/components/TestComponent/index.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+class TestComponent extends PureComponent{
+  render(){
+    return(
+      <div>Test component</div>
+    )
+  }
+}
+
+export default TestComponent;
---
+++ src/store.js
@@ -0,0 +1,7 @@
+import { createStore, applyMidleware, compose } from 'redux';
+import rootReducer from './reducers';
+
+export const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
